24ddf9f69088ba84623ba06e93b260a4418af860
Sebastian Sanokowski <sebastiansanokowski@gmail.com>
2024-12-04 09:36:28 +0100

    network is now more similar to other diffuson sampler papers, shared embedding seems to elad to mdoe collapse in log variance loss

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import torch
 ### TODO make seperate run configs for discrete time and continuous time
 
 parser = argparse.ArgumentParser(description="Denoising Diffusion Sampler")
-parser.add_argument("--GPU", type=str, default="6", help="GPU id to use")
+parser.add_argument("--GPU", type=int, default=6, help="GPU id to use")
 parser.add_argument("--SDE_Loss", type=str, default="LogVariance_Loss", choices=["Reverse_KL_Loss","LogVariance_Loss", "LogVariance_Loss_MC", "LogVariance_Loss_with_grad", "LogVariance_Loss_weighted",
                                                                                 "Discrete_Time_rKL_Loss_log_deriv", "Discrete_Time_rKL_Loss_reparam"], help="select loss function")
 parser.add_argument("--SDE_Type", type=str, default="VP_SDE", choices=["VP_SDE", "subVP_SDE"], help="GPU id to use")
