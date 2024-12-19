8a5efb06144d095878e4b9263271f6d7d7e434df
Sebastian Lehner <s.lehner@gmx.at>
2024-12-19 14:54:25 +0100

    fix bug in call of simulate_reverse_sde_scan after training

--- Trainer/train.py
+++ Trainer/train.py
@@ -223,7 +223,7 @@ class TrainerClass:
         self.SDE_LossClass.SDE_params = param_dict["SDE_params"]
 
         n_samples = self.config["n_eval_samples"]
-        SDE_tracer, out_dict, key = self.SDE_LossClass.simulate_reverse_sde_scan( params, self.SDE_LossClass.Energy_params, self.SDE_LossClass.SDE_params, key, n_integration_steps = self.n_integration_steps, n_states = n_samples)
+        SDE_tracer, out_dict, key = self.SDE_LossClass.simulate_reverse_sde_scan( params, self.SDE_LossClass.Energy_params, self.SDE_LossClass.SDE_params, T_curr, key, n_integration_steps = self.n_integration_steps, n_states = n_samples)
         
         #TODO implement EnergyClass dependent eval plots by giving the entire SDE_tracer to the plotting functions and implementing custom plotting functions in each EnergyClass
         if self.EnergyClass.config["name"] == "GaussianMixture":  
