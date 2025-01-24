1059f70549b3214e8d0cdc53b6737b6a548c9760
Sebastian Lehner <s.lehner@gmx.at>
2025-01-24 12:50:47 +0100

    fix GMM

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ parser.add_argument("--model_seed", type=int, default=0, help="Seed used for mod
 
 #energy function specific args
 parser.add_argument("--Pytheus_challenge", type=int, default=1, choices=[0,1,2,3,4,5], help="Pyhteus Chellange Index")
-parser.add_argument("--Scaling_factor", type=float, default=1., help="Scaling factor for Energy Functions")
+parser.add_argument("--Scaling_factor", type=float, default=40., help="Scaling factor for Energy Functions")
 parser.add_argument("--Variances", type=float, default=0.1, help="Variances of Gaussian Mixtures before scalling when means ~Unif([-1,1])")
 
 
@@ -160,12 +160,7 @@ if(__name__ == "__main__"):
             }
         else:
             #modified sampling distributions are only applicable for certain losses
-<<<<<<< HEAD
             if(args.use_off_policy and (args.SDE_Loss != "LogVariance_Loss" and args.SDE_Loss != "Bridge_LogVarLoss" and args.SDE_Loss != "Reverse_KL_Loss_logderiv" and args.SDE_Loss != "Bridge_rKL_logderiv")):
-=======
-
-            if(args.use_off_policy and (args.SDE_Loss != "LogVariance_Loss" and args.SDE_Loss != "Bridge_LogVarLoss" and args.SDE_Loss != "Reverse_KL_Loss_logderiv")):
->>>>>>> 8721b27c4481ab96d392b9fb0cc4b5af67bce5b6
                 raise ValueError("Off policy only implemented for LogVariance_Loss")
             if(not args.use_off_policy and args.sigma_scale_factor != 0):
                 raise ValueError("Sigma scale factor != 0 and use_off_policy is off")
@@ -231,12 +226,12 @@ if(__name__ == "__main__"):
                 loc_scaling = args.Scaling_factor
                 var_scaling = args.Variances
                 mean = (torch.rand((num_gaussians, dim)) - 0.5)*2*loc_scaling
-                variances = torch.ones((num_gaussians, dim)) * var_scaling
+                log_var = torch.ones((num_gaussians, dim)) * var_scaling
                 Energy_Config = {
                     "name": "GaussianMixture",
                     "dim_x": dim,
                     "means": mean,
-                    "variances": variances,
+                    "variances": torch.nn.functional.softplus(log_var),
                     "weights": [1/num_gaussians for i in range(num_gaussians)],
                     "num_modes": num_gaussians
                 }
