plot 2D trajectories with the covariance ellipses, fix some naming in the plots

Trainer/train.py
@@ -207,7 +207,11 @@ class TrainerClass:
 
     def plot_figures(self, SDE_tracer, epoch, sample_mode = "train"):
         overall_dict = {}
-        fig_traj_dict = self.EnergyClass.plot_trajectories(np.array(SDE_tracer["ys"])[:,0:10,:])
+        if "noise_stds" in SDE_tracer and "sigma_prior" in SDE_tracer:
+            fig_traj_dict = self.EnergyClass.plot_trajectories(np.array(SDE_tracer["ys"])[:,0:10,:], np.array(SDE_tracer["noise_stds"])[:,0:10,:], np.array(SDE_tracer["sigma_prior"]))
+        else:
+            fig_traj_dict = self.EnergyClass.plot_trajectories(np.array(SDE_tracer["ys"])[:,0:10,:])
+
         fig_hist_dict = self.EnergyClass.plot_histogram(np.array(SDE_tracer["y_final"]))
         fig_last_samples_dict = self.EnergyClass.plot_last_samples(np.array(SDE_tracer["y_final"]))
 
@@ -305,7 +309,7 @@ class TrainerClass:
             n_samples = self.config["n_eval_samples"]
             SDE_tracer, out_dict, key = self.SDE_LossClass.simulate_reverse_sde_scan( params, self.SDE_LossClass.Interpol_params, self.SDE_LossClass.SDE_params, T_curr, key, sample_mode = "eval", n_integration_steps = self.n_integration_steps, n_states = n_samples)
             
-            self.plot_figures(SDE_tracer, self.num_epochs+1)
+            self.plot_figures(SDE_tracer, self.num_epochs+1, sample_mode= "ckpt_best_sinkhorn")
         except:
             pass
 