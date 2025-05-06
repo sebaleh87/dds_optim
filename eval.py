96a5ecd78b8410243028bb0a27c104c042bc61f4
Sebastian Sanokowski <sebastiansanokowski@gmail.com>
2025-05-06 13:56:57 +0200

    update eval

--- eval.py
+++ eval.py
@@ -6,30 +6,8 @@ from tqdm.auto import tqdm
 import argparse
 from Trainer.train import TrainerClass
 import jax
-# def go_deeper_or_add_key(config, padd_keys, padd_key):
-#     if(isinstance(config[padd_key], dict)):
-#         go_deeper_or_add_key(config[padd_key], padd_keys[padd_key], padd_key)
-#     else:
-#         for sub_key in padd_key_dict[padd_key]:
-#             if sub_key not in config[key]:
-#                 config[key][sub_key] = padd_key_dict[key][sub_key]
-#                 print(key, sub_key, "added to config")
-
-# def config_completer(config):
-#     ### adds keys to default if key is missing
-#     padd_key_dict = {"SDE_Loss_Config": {"SDE_Type_Config":{"natural_gradient": False}}}
-
-#     for key in config:
-#         if key in padd_key_dict.keys():
-#             if(isinstance(config[key], dict)):
-                
-#             else:
-#                 for sub_key in padd_key_dict[key]:
-#                     if sub_key not in config[key]:
-#                         config[key][sub_key] = padd_key_dict[key][sub_key]
-#                         print(key, sub_key, "added to config")
-
-#     return config
+import jax.numpy as jnp
+from Configs import run_wandb_ids
 
 
 def load_params_and_config(wandb_run_name, filename = "params_and_config_train_end.pkl"):
@@ -80,18 +58,22 @@ class EvaluatorClass(TrainerClass):
         self.SDE_LossClass.Energy_params = param_dict["Energy_params"]
         self.SDE_LossClass.SDE_params = param_dict["SDE_params"]
 
+    def compute_MMD_and_Sinkhorn(self, n_eval_samples):
+        pass
+
     def chunk_evaluate(self, n_eval_samples, chunk_size=10):
         """
         Evaluate the model in a specified number of chunks.
         """
         temp = 1.
         n_chunks = chunk_size
-        samples_per_chunk = n_eval_samples // n_chunks
-        leftover = n_eval_samples % n_chunks
+        samples_per_chunk = n_eval_samples
+        leftover = 0# n_eval_samples % n_chunks
 
         aggregated_metrics = {}
         key = jax.random.PRNGKey(0)
         combined_tracer = {}
+        IPM_Metrics = {"MMD": [], "Sinkhorn": []}
 
         for chunk_idx in tqdm(range(n_chunks), desc="Evaluating in chunks"):
             curr_chunk_size = samples_per_chunk
@@ -105,8 +87,14 @@ class EvaluatorClass(TrainerClass):
                 temp, key, sample_mode = "train",
                 n_integration_steps=self.n_integration_steps,
                 n_states=curr_chunk_size
-            )
+            )  
 
+            model_samples = out_dict["X_0"]
+
+            out_dict = self.sd_calculator.compute_MMD_and_Sinkhorn(model_samples)
+            IPM_Metrics["MMD"].append(jnp.sqrt(out_dict["MMD^2"]))
+            IPM_Metrics["Sinkhorn"].append(out_dict["Sinkhorn divergence"])
+            
             if not combined_tracer:
                 combined_tracer = {k: [] for k in SDE_tracer.keys()}
 
@@ -134,6 +122,12 @@ class EvaluatorClass(TrainerClass):
                 except ValueError:
                     final_metrics[k] = np.mean([np.mean(x) for x in v_list])
 
+        print("Sinkhorn Distance list", IPM_Metrics["Sinkhorn"])
+        final_metrics["MMD"] = {"mean": np.mean(IPM_Metrics["MMD"]), "std": np.std(IPM_Metrics["MMD"])/ jnp.sqrt(len(IPM_Metrics["MMD"]))}
+        final_metrics["Sinkhorn"] = {"mean": np.mean(IPM_Metrics["Sinkhorn"]), "std": np.std(IPM_Metrics["Sinkhorn"])/ jnp.sqrt(len(IPM_Metrics["Sinkhorn"]))}
+
+        print(final_metrics["MMD"], "MMD")
+        print(final_metrics["Sinkhorn"], "Sinkhorn")
         return final_metrics
 
     def generate_plots(self):
@@ -173,34 +167,53 @@ def parse_energy_config_array(config_dict, key):
         raise
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--wandb_run_name", default = "dry-silence-17", type=str, help="Wandb run name of run to evaluate")
-    parser.add_argument("--checkpoint_metric", default = "params_and_config_train_end.pkl", choices=["params_and_config_train_end.pkl","Free_Energy_at_T=1", "Sinkhorn"], type=str, help="Wandb run name of run to evaluate")
-    parser.add_argument("--n_eval_samples", type=int, default=10000)
-    parser.add_argument("--chunk_size", type=int, default=1000)
-    parser.add_argument("--GPU", type=int, default=0)
-    args = parser.parse_args()
-
-    # Set GPU
-    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" 
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.GPU)
-
-    params, config = load_params_and_config(args.wandb_run_name)
+def evaluate_on_run(wandb_run_name, n_eval_samples, chunk_size):
+    params, config = load_params_and_config(wandb_run_name)
     
     if "EnergyConfig" in config:
         for key in ["means", "variances"]:
             if key in config['EnergyConfig']:
                 parse_energy_config_array(config['EnergyConfig'], key)
 
-    config.update({"n_eval_samples": args.n_eval_samples})
+    config.update({"n_eval_samples": n_eval_samples})
 
     #config = config_completer(config)
-    evaluator = EvaluatorClass(config, args.wandb_run_name, params)
+    evaluator = EvaluatorClass(config, wandb_run_name, params)
 
-    metrics = evaluator.chunk_evaluate(args.n_eval_samples, args.chunk_size)
-    wandb.log({ f"eval/{key}": metrics[key] for key in metrics.keys()})
-    evaluator.generate_plots()
+    metrics = evaluator.chunk_evaluate(n_eval_samples, chunk_size)
+    #wandb.log({ f"eval/{key}": metrics[key] for key in metrics.keys()})
+    #evaluator.generate_plots()
+    return {"MMD": metrics["MMD"], "Sinkhorn": metrics["Sinkhorn"]}
+
+
+def evaluate_runs(wandb_ids):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoint_metric", default = "params_and_config_train_end.pkl", choices=["params_and_config_train_end.pkl","Free_Energy_at_T=1", "Sinkhorn"], type=str, help="Wandb run name of run to evaluate")
+    parser.add_argument("--n_eval_samples", type=int, default=16000)
+    parser.add_argument("--chunk_size", type=int, default=20)
+    parser.add_argument("--GPU", type=int, default=0)
+    args = parser.parse_args()
+
+    # Set GPU
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" 
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.GPU)
+    
+    metric_dict = { "MMD": [], "Sinkhorn": []}
+
+    for wandb_id in wandb_ids:
+        metrix_dict_per_run = evaluate_on_run(wandb_id, args.n_eval_samples, args.chunk_size)
+        metric_dict["MMD"].append(metrix_dict_per_run["MMD"]["mean"])
+        metric_dict["Sinkhorn"].append(metrix_dict_per_run["Sinkhorn"]["mean"])
+
+    print(metric_dict["Sinkhorn"])
+    print("Sinkhorn", np.mean(metric_dict["Sinkhorn"]) , np.std(metric_dict["Sinkhorn"]))
+    print("MMD", np.mean(metric_dict["MMD"]) , np.std(metric_dict["MMD"]))
 
 if __name__ == "__main__":
-    main() 
+
+    ### GMM 
+    # log_derivative 
+    GMM_rKL_LD = ["clone-trooper-25", "legendary-bantha-29", "jedi-fighter-33", "galactic-speeder-37", "tusken-republic-41"
+                    ,"grievous-carrier-45", "carbonite-lightsaber-48"]
+
+    evaluate_runs(GMM_rKL_LD) 
